a13f17ce4a522abb6e4f6495e9b2915576dffe47
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-05-28 14:29:46 -0300

    Hard coded o url base da api

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: import.meta.env.VITE_API_BASE_URL });
+const axiosInstance = axios.create({ baseURL: "https://backend-kqnasaoyha-uc.a.run.app" });
 
 export default axiosInstance;
