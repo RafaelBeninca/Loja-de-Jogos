fe6fed66136084806a02016b3a87ca0b3f916bf3
RafaelTeixeiraSerafim <rafael.teixeiraserafim@gmail.com>
2024-06-02 23:08:36 -0300

    Mudança do baseURL do axios para produção

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // For development
-const baseURL = "http://localhost:5000";
+// const baseURL = "http://localhost:5000";
 
 // For deployment
-// const baseURL = "https://backend-kqnasaoyha-uc.a.run.app";
+const baseURL = "https://backend-kqnasaoyha-uc.a.run.app";
 
 const axiosInstance = axios.create({ baseURL: baseURL });
 
